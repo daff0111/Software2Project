6e03b46671f1862c4b04c470274743652750179b
mfim <matheusfim@hotmail.com>
2017-06-17 16:21:16 +0200

    [ACA]Added some documentation to be used as guide and still non working version of the wrapper
    Signed-off-by: mfim <matheusfim@hotmail.com>

--- ACA Project/API_CPP/include/OpenCl_wrap.h
+++ ACA Project/API_CPP/include/OpenCl_wrap.h
@@ -27,11 +27,44 @@
 extern "C" {
 #endif 
 
-//typedef struct cl_context
+/* ************************************************ */
+
+/* scalar types */
+typedef signed   __int32        cl_int;
+typedef unsigned __int64        cl_ulong;
+typedef cl_ulong            cl_bitfield;
+typedef cl_bitfield         cl_mem_flags;
+//typedef unsigned __int32        cl_uint;
+
+/* abstract types */
+typedef struct _cl_context *        cl_context;
+typedef struct _cl_device_id *      cl_device_id;
+typedef struct _cl_kernel *         cl_kernel;
+typedef struct _cl_program *        cl_program;
+typedef struct _cl_command_queue *  cl_command_queue;
+typedef struct _cl_event *          cl_event;
+typedef struct _cl_mem *            cl_mem;
+//typedef struct _cl_platform_id *    cl_platform_id;
+
+
+//typedef intptr_t            cl_context_properties;
+
+/* ************************************************ */ 
 
 
 /*! Selects mango platform and then initializes the context  */
 cl_context CreateContext();
+
+cl_program CreateProgram(cl_context context, cl_device_id device, 
+                         const char* fileName);
+
+cl_kernel clCreateKernel(cl_program program, const char *kernel_name, cl_int *errcode_ret);
+
+cl_mem clCreateBuffer(cl_context context, cl_mem_flags flags, size_t size, void *host_ptr, cl_int *errcode_ret
+	);
+
+clReleaseProgram();
+clReleaseMemObject();
 
 
 #ifdef __cplusplus
--- ACA Project/API_CPP/src/opencl_wrap.cpp
+++ ACA Project/API_CPP/src/opencl_wrap.cpp
@@ -6,19 +6,75 @@ static mango::Context *cxt;
 
 extern "C"{
 
+int read_buffer = 0;
+int write_buffer = 0;
+
+/* This is the only createcontext function in the OpenCL lib that suits our needs, since mango does not need to handle different platforms */ 
 cl_context CreateContext(){
 
-	cl_context *context;
-
+	// this is the mango instance
 	cxt = new mango::Context;
+
+	// it used as a dummy, as the context concept is different between both platforms
+	return 1;
+}
+
+/* program here is handled as a kernel function */ 
+cl_program CreateProgram(cl_context context, cl_device_id device, 
+                         const char* fileName){
+
+	// openCL return
+
+	// Mango wrap
+	kernelfunction *k = mango_kernel_function_init();
+	mango_load_kernel(filename, k, GN, BINARY);
 	
-	context = malloc (sizeof(cl_context));
+	// still need to try if it's compatible
+  	return (cl_program) k;
+}
 
-	context = cxt. 
+/*
+/* NOOOOOT FINISHED!!!!! 
+/* cl_mem is used as mango_buffer  
+cl_mem clCreateBuffer(cl_context context, cl_mem_flags flags, size_t size, void *host_ptr, cl_int *errcode_ret){
+
+	cl_int err = CL_SUCESS;
+	mango_buffer_t b;
+
+	/* This flag is valid only if host_ptr is not NULL 
+	if (UNLIKELY((flags & CL_MEM_COPY_HOST_PTR || flags & CL_MEM_USE_HOST_PTR) && host_ptr == NULL)) {
+    	err = CL_INVALID_HOST_PTR;
+   		goto error;
+  	}
+
+  	/* this mode of allocation is not available to be used with mango 
+  	if (UNLIKELY(flags & CL_MEM_ALLOC_HOST_PTR)) {
+    	err = CL_INVALID_HOST_PTR;
+    	goto error;
+  	}
+
+  	/* CL_MEM_COPY_HOST_PTR and CL_MEM_USE_HOST_PTR are mutually exclusive. 
+  	if (UNLIKELY(flags & CL_MEM_COPY_HOST_PTR && flags & CL_MEM_USE_HOST_PTR)) {
+    	err = CL_INVALID_HOST_PTR;
+    	goto error;
+  	}
+
+  	b = mango_register_memory(, size, BUFFER, (flags & CL_MEM_WRITE_ONLY  || flags & CL_MEM_READ_WRITE)  , (flags & CL_MEM_READ_ONLY || flags & CL_MEM_READ_WRITE), context->);
 
 
-	return ;
+  	if (flags & CL_MEM_USE_HOST_PTR || flags & CL_MEM_COPY_HOST_PTR)
+//    mem->host_ptr = data;
+
+exit:
+	if (errcode_ret)
+		*errcode_ret = err;
+	return (cl_mem) b;
+error:
+  	b = NULL;
+  	goto exit;
 
 }
+*/
+
 
 }
